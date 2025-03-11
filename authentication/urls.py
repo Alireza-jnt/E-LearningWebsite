c0128ea9ba54aa8282468e9e5ca267a6e8c00d3d
Alireza-j <alireza_jnt@outlook.com>
2025-03-11 16:17:30 +0330

    creating userprofile endpoints urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,15 @@
 from rest_framework_simplejwt.views import TokenBlacklistView, TokenObtainPairView
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserProfileViewSet, UserRegistrationView
 
+router = DefaultRouter()
+router.register(r'profile', UserProfileViewSet, basename='profile')
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='signup'),
     # path('logout/', views.LogoutView.as_view(), name='logout'),
     path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path('', include(router.urls)),
+    path('register/', UserRegistrationView.as_view(), name='register'),
 ]
